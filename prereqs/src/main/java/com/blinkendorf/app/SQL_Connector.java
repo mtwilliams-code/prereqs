5c7cdea43df0a73ad827132032517bd7a704bd49
mtwilliams <matthew@m5williams.info>
2018-02-12 13:32:00 -0600

    improved SQL_Connector

--- prereqs/src/main/java/com/blinkendorf/app/SQL_Connector.java
+++ prereqs/src/main/java/com/blinkendorf/app/SQL_Connector.java
@@ -9,16 +9,26 @@ import java.sql.SQLException;
 
 public class SQL_Connector {
 
-  String url = "jdbc:mysql://localhost:3306/registration";
+  String url = "jdbc:mysql://localhost:3306/records";
   String username = "java";
   String password = "Java";
+  Connection conn = null;
 
+  /**
+  * Creates and opens a connection to a mySQL server located at the 
+  * given URL with the given username and password.
+  *
+  * Defaults to database "records" on localhost with username java and password Java.
+  *
+  * @param  url the URL of the database, of the form "jdbc:mysql://host:port/databasename"
+  * @param  username the username to connect to the mySQL server with
+  * @param  password the password to connect to the mySQL server with
+  * @return      the image at the specified URL
+  */
   public SQL_Connector() throws Exception {
     System.out.println("Connecting database...");
-    Connection conn = null;
     try {
-      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/registration" + "user=java&password=Java");
-
+      conn = DriverManager.getConnection(url, username, password);
       // Do something with the Connection
 
     } catch (SQLException ex) {
@@ -27,6 +37,45 @@ public class SQL_Connector {
       System.out.println("SQLState: " + ex.getSQLState());
       System.out.println("VendorError: " + ex.getErrorCode());
       throw new Exception(ex.getMessage());
+    }
+  }
+
+  /**
+  * Creates and opens a connection to a mySQL server located at the 
+  * given URL with the given username and password.
+  *
+  * Defaults to database "records" on localhost with username java and password Java.
+  *
+  * @param  url the URL of the database, of the form "jdbc:mysql://host:port/databasename"
+  * @param  username the username to connect to the mySQL server with
+  * @param  password the password to connect to the mySQL server with
+  * @return      the image at the specified URL
+  */
+  public SQL_Connector(String url, String username, String password) throws Exception {
+    System.out.println("Connecting database...");
+    this.url = url;
+    this.username = username;
+    this.password = password;
+    try {
+      conn = DriverManager.getConnection(url, username, password);
+      // Do something with the Connection
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+      throw new Exception(ex.getMessage());
+    }
+  }
+
+  public void closeConnection() throws Exception {
+    try{
+      conn.close();   
+    }
+    catch(Exception e)
+    {
+      throw e;
     }
   }
 
