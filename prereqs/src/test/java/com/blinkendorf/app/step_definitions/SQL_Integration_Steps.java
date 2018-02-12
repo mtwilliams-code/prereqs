e10386cfaaab6b55fc9b0ac4e788b0ebb77376e8
mtwilliams <matthew@m5williams.info>
2018-02-12 12:54:49 -0600

    moved SQL_Intergration_Steps so that cucumber could find it and added imports

--- prereqs/src/test/java/com/blinkendorf/app/step_definitions/SQL_Integration_Steps.java
+++ prereqs/src/test/java/com/blinkendorf/app/step_definitions/SQL_Integration_Steps.java
@@ -1,7 +1,19 @@
-package com.blinkendorf.step_definitions;
+package com.blinkendorf.app.step_definitions;
 
 
+import cucumber.api.java.en.*;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.PendingException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.blinkendorf.app.SQL_Connector;
+import java.nio.file.Files;
+import java.nio.file.*;
 
 public class SQL_Integration_Steps {
 
