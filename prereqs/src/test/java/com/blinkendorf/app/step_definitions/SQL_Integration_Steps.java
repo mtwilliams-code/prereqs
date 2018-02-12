c73b61791c823b2b2a9b56713b7b1faac5979b08
mtwilliams <matthew@m5williams.info>
2018-02-12 13:10:09 -0600

    completed test to see if mySQL server is running

--- prereqs/src/test/java/com/blinkendorf/app/step_definitions/SQL_Integration_Steps.java
+++ prereqs/src/test/java/com/blinkendorf/app/step_definitions/SQL_Integration_Steps.java
@@ -14,13 +14,20 @@ import static org.junit.Assert.assertNotNull;
 import com.blinkendorf.app.SQL_Connector;
 import java.nio.file.Files;
 import java.nio.file.*;
+import java.net.*;
 
 public class SQL_Integration_Steps {
 
   @Given("^a mySQL server is running on the local machine$")
   public void a_mySQL_server_is_running_on_the_local_machine() throws Exception {
     // Write code here that turns the phrase above into concrete actions
-    throw new PendingException();
+    // checks whethere ip address is reachable - does not check if server is running
+    InetAddress addr = InetAddress.getByName("localhost");
+    // addr.isReachable(2000); //tries for two seconds to connect to localhost. Should be plenty of time.
+    int port = 3306;
+    SocketAddress sockaddr = new InetSocketAddress(addr, port);
+    Socket sock = new Socket();
+    sock.connect(sockaddr, 2000); // open the connection with a 2 seconds timeout
   }
 
   @Given("^the app is not already connected to a server$")
