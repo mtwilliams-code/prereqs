c154dd487230280b0148e3ef0311014d7e30d36e
mtwilliams <matthew@m5williams.info>
2018-02-12 12:48:05 -0600

    typo fix

---
+++ prereqs/src/test/java/com/blinkendorf/step_definitions/SQL_Integration_Steps.java
@@ -0,0 +1,31 @@
+package com.blinkendorf.step_definitions;
+
+
+
+
+public class SQL_Integration_Steps {
+
+  @Given("^a mySQL server is running on the local machine$")
+  public void a_mySQL_server_is_running_on_the_local_machine() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Given("^the app is not already connected to a server$")
+  public void the_app_is_not_already_connected_to_a_server() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @When("^the app tries to connect to the local server$")
+  public void the_app_tries_to_connect_to_the_local_server() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+
+  @Then("^the connection should be valid$")
+  public void the_connection_should_be_valid() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+  }
+}
